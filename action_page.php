e3c0c01af13402a39780d140cef6c4a4b7790baa
YourEternalHeadache <jacobmmendez@outlook.com>
2024-11-17 11:52:43 -1000

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,1 +1,37 @@
+<html>
+  <head>
+	<title>Want to Know More Information</title>
+  <script type="text"/javascript">
+    function GatherInfo()
+    {
 
+    }
+  </script>
+  </head>
+  <body>
+    First Name: <input type="text" id="fname" /><br />
+    Last Name: <input type="text" id="lname" /><br />
+    Age: <input type="text" id="age" /><br />
+    Phone Number: <input type="text" id="pnum" /><br />
+    E-Mail: <input type="text" id="email" /><br />
+    Male <input type="radio" id="male" /><br />
+    Female <input type="radio" id="female" /><br />
+    Other <input type="radio" id="other" /><br />
+    Yes <input type="radio" id="yes" /><br />
+    No <input type="radio" id="no" /><br />
+    Denmark <input type="checkbox" id=denmark /><br>
+    Germany <input type="checkbox" id=germany /><br>
+    England <input type="checkbox" id=england /><br>
+    France <input type="checkbox" id=france /><br>
+    Spain <input type="checkbox" id=spain /><br>
+    Italy <input type="checkbox" id=italy /><br>
+    Switzerland <input type="checkbox" id=switzerland /><br>
+    Norway <input type="checkbox" id=norway /><br>
+    Sweden <input type="checkbox" id=sweden /><br>
+    Finland <input type="checkbox" id=finland /><br>
+    Poland <input type="checkbox" id=poland /><br>
+    Scotland <input type="checkbox" id=scotland /><br>
+    None of the above <input type="checkbox" id=none /><br>
+    <input type="button" value="Process" onclick="GatherInfo();" />
+  </body>
+</html>
