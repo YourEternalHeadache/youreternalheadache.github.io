8d6a14038947d8f37ed767dbb655adbd8ec4d02b
YourEternalHeadache <jacobmmendez@outlook.com>
2024-11-17 12:45:53 -1000

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,59 +1,24 @@
-<html>
-  <head>
-	<title>Want to Know More Information</title>
-  <script type="text"/javascript">
-    function GatherInfo()
-    {
-	first = document.getElementById("fname").value;
-	last = document.getElementById("lname").value;
-	age = parseInt(document.getElementById("age").value);
-	phone = parseInt(document.getElementById("pnum").value);
-	email = document.getElementById("email").value;
-	male = document.getElementById("male").value;
-	female = document.getElementById("female").value;
-	other = document.getElementById("other").value;
-	yes = document.getElementById("yes").value;
-	no = document.getElementById("no").value;
-	denmark = document.getElementById("denmark").value;
-	germany = document.getElementById("germany").value;
-	england = document.getElementById("england").value;
-	france = document.getElementById("france").value;
-	spain = document.getElementById("spain").value;
-	italy = document.getElementById("italy").value;
-	switzerland = document.getElementById("switzerland").value;
-	norway = document.getElementById("norway").value;
-	sweden = document.getElementById("sweden").value;
-	finland = document.getElementById("finland").value;
-	poland = document.getElementById("poland").value;
-	scotland = document.getElementById("scotland").value;
-	none = document.getElementById("none").value;
-    }
-  </script>
-  </head>
-  <body>
-    First Name: <input type="text" id="fname" /><br />
-    Last Name: <input type="text" id="lname" /><br />
-    Age: <input type="text" id="age" /><br />
-    Phone Number: <input type="text" id="pnum" /><br />
-    E-Mail: <input type="text" id="email" /><br />
-    Male <input type="radio" id="male" /><br />
-    Female <input type="radio" id="female" /><br />
-    Other <input type="radio" id="other" /><br />
-    Yes <input type="radio" id="yes" /><br />
-    No <input type="radio" id="no" /><br />
-    Denmark <input type="checkbox" id=denmark /><br>
-    Germany <input type="checkbox" id=germany /><br>
-    England <input type="checkbox" id=england /><br>
-    France <input type="checkbox" id=france /><br>
-    Spain <input type="checkbox" id=spain /><br>
-    Italy <input type="checkbox" id=italy /><br>
-    Switzerland <input type="checkbox" id=switzerland /><br>
-    Norway <input type="checkbox" id=norway /><br>
-    Sweden <input type="checkbox" id=sweden /><br>
-    Finland <input type="checkbox" id=finland /><br>
-    Poland <input type="checkbox" id=poland /><br>
-    Scotland <input type="checkbox" id=scotland /><br>
-    None of the above <input type="checkbox" id=none /><br>
-    <input type="button" value="Process" onclick="GatherInfo();" />
-  </body>
-</html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fname = htmlspecialchars($_POST['fname']);
+    $lname = htmlspecialchars($_POST['lname']);
+    $age = htmlspecialchars($_POST['age']);
+    $pnum = htmlspecialchars($_POST['pnum']);
+    $email = htmlspecialchars($_POST['email']);
+    $gender = htmlspecialchars($_POST['gender']);
+    $beenToEurope = htmlspecialchars($_POST['beenToEurope']);
+    $beenTo = $_POST['beenTo'];
+	
+    echo "Form submitted successfully! Here is the data:<br>";
+    echo "First Name: $fname<br>";
+    echo "Last Name: $lname<br>";
+    echo "Age: $age<br>";
+    echo "Phone Number: $pnum<br>";
+    echo "Email: $email<br>";
+    echo "Gender: $gender<br>";
+    echo "Been to Europe: $beenToEurope<br>";
+    echo "Places visited: " . implode(", ", $beenTo) . "<br>";
+} else {
+    echo "Invalid request method!";
+}
+?>
